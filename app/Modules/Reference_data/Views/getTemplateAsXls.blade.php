80a8d2bfc347de99977255c194e7ffaf69bcc2f0
nakia <nakia@iglobal.co.id>
2023-10-29 10:43:51 +0700

    Template Excel

--- app/Modules/Reference_data/Views/getTemplateAsXls.blade.php
+++ app/Modules/Reference_data/Views/getTemplateAsXls.blade.php
@@ -2,23 +2,76 @@
     <tr>
         <td class="red-text" colspan="10"><b><u>Mohon diperhatikan</u></b></td>
     </tr>
+    <?php
+        $no = 0;
+        $colspan = 14;
+        $citys = \Models\city::pluck('code')->all();
+        $citys = sprintf('"%s"', implode('","', $citys));
+        $districts = \Models\district::orderBy('id','asc')->pluck('code')->all();
+        $districts = sprintf('"%s"', implode('","', $districts));
+        $subdistricts = \Models\subdistrict::orderBy('district_id','asc')
+            ->orderBy('id','desc')
+            ->get()
+            ->groupBy('district.code')
+            ->all();
+        $religions = \Models\religion::orderBy('id','asc')->pluck('code')->all();
+        $religions = sprintf('"%s"', implode('","', $religions));
+    ?>
     <tr>
-        <td></td>
-        <td colspan="10">1. Dilarang merubah format excel yang telah disediakan</td>
+        <td width="10px" style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">Dilarang merubah format excel yang telah disediakan</td>
     </tr>
     <tr>
-        <td></td>
-        <td colspan="10">2. Dilarang merubah setiap judul tabel yang ada</td>
+        <td style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">Dilarang merubah setiap judul tabel yang ada</td>
     </tr>
     <tr>
-        <td></td>
-        <td colspan="10">3.
+        <td style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">
             Pastikan simpan format excel yang telah diisi Save As sebagai Microsoft Excel 2007/2003 (.xls)
         </td>
     </tr>
     <tr>
+        <td style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">
+            Kolom "Wilayah" diisi kode wilayah ({{ $citys }}) tanpa tanda petik
+        </td>
+    </tr>
+    <tr>
+        <td style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">
+            Kolom "Kecamatan" diisi kode kecamatan ({{ $districts }}) tanpa tanda petik
+        </td>
+    </tr>
+    <tr>
+        <td style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">
+            Kolom "Kelurahan" diisi kode kelurahan berdasarkan kecamatan tanpa tanda petik
+        </td>
+    </tr>
+    <tr>
         <td></td>
-        <td colspan="10"></td>
+        <td colspan="{{ $colspan }}">
+            @foreach($subdistricts as $district_code => $subdistrict)
+                "{{ $district_code }}" :
+                {{ sprintf('"%s"', implode('","', $subdistrict->pluck('code')->all())) }}<br>
+            @endforeach
+        </td>
+    </tr>
+    <tr>
+        <td style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">
+            Kolom "Jenis Kelamin" diisi kode ("L"/"P") tanpa tanda petik
+        </td>
+    </tr>
+    <tr>
+        <td style="mso-number-format: \@;text-align:center">{{ ++$no }}.</td>
+        <td colspan="{{ $colspan }}">
+            Kolom "Agama" diisi kode ({{ $religions }}) tanpa tanda petik
+        </td>
+    </tr>
+    <tr>
+        <td colspan="{{ $colspan }}"></td>
     </tr>
 </table>
 <table border="1" style="border-collapse: collapse;">
@@ -26,15 +79,31 @@
         <tr style="background: #e5e5e5;">
             <th width="10px">No</th>
             <th>NIK</th>
-            <th>Nama</th>
+            <th>Nama Lengkap</th>
             <th>Wilayah</th>
             <th>Kecamatan</th>
             <th>Kelurahan</th>
-            <th>Koordinator</th>
+            <th>Nomor TPS</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Jenis Kelamin</th>
+            <th>Agama</th>
+            <th>Pekerjaan</th>
+            <th>Alamat</th>
+            <th>RT</th>
+            <th>RW</th>
         </tr>
     </thead>
     <tbody>
         <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td></td>
             <td></td>
             <td></td>
