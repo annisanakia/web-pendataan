b8af0ba7ebb9ac886790fe14785a964764491286
nakia <nakia@iglobal.co.id>
2024-02-04 10:00:51 +0700

    Lapor hasil pemilu

--- app/Modules/Home/Views/index_coordinator_tps.blade.php
+++ app/Modules/Home/Views/index_coordinator_tps.blade.php
@@ -7,9 +7,22 @@
     </ol>
     <div class="row">
         <div class="col-lg-12">
-            <div class="alert alert-warning text-center">
+            <div class="alert alert-warning text-center mb-4">
                 <h2>Real Count</h2>
                 <h4 id="countdown">0</h4>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-grid gap-3 mt-3 mb-md-0 mb-3 d-md-flex align-items-center">
+                        <img class="mx-md-0 mx-auto" src="{{ asset('assets/images/warning.png') }}">
+                        <h4 class="">Lapor hasil pemilu sekarang juga!</h4>
+                    </div>
+                    <div class="d-grid d-md-flex justify-content-md-end">
+                        <a href="{{ url('election_results') }}" class="btn btn-danger btn-lg">
+                            Lapor sekarang
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
